792a3b46c14067383da08e33fab1b81489055266
Hannah Park <hye.na.pa@gmail.com>
2018-09-19 19:27:41 -0700

    init commit

---
+++ JS/app.js
@@ -0,0 +1,40 @@
+/*----- constants -----*/
+const PLAYER_ONE
+const PLAYER_TWO
+
+/*----- app's state (variables) -----*/
+var board = [null, null .....];
+var playerTurn; //keeping track of who's turn
+var gameOver; //player wins or loses
+
+
+/*----- cached element references (refer back to and reuse) -----*/
+$('td')
+
+/*----- event listeners -----*/
+board.click(function) //user clicks on board on any empty slot <td>
+
+
+/*----- functions (be able to reuse them in multiple different places) -----*/
+//INIT FUNCTION
+//call whatever this function to use piece
+
+
+//DURING GAMEPLAY
+// * create your own render function. using methods, or build-in functions, like .push
+// * player changes turn
+var checkWinner //to see if all four pieces connect
+//do animation or celebratory text
+
+
+//END OF GAME
+var resetGame; //clears the board the board at any time
+
+
+
+//PSEUDOCODE
+//two players. one player selects piece
+//player one selects empty slot
+//drop pieces
+//check for the winner
+
